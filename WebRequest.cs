1daacf505e08fdd5389016359d84b159006a5323
Michael Gamlem III <mgamlem3@gmail.com>
2018-11-28 10:50:47 -0800

    Initial Commit

---
+++ WebRequest.cs
@@ -0,0 +1,12 @@
+using System;
+
+public class WebRequest
+{
+    public WebRequest()
+    {
+    }
+
+    // gets content of wikipedia page
+    // input: string of url or string to search
+    // output: new webpage object
+}
---
+++ Webpage.cs
@@ -0,0 +1,10 @@
+using System;
+using System.Collections.Generic;
+public class Webpage {
+    private string Title { get; set; }
+    public List<string> Links { get; set; }
+
+     public Webpage() {}
+
+     public Webpage()
+}
